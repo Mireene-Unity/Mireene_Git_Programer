5e50285ee2dd27b11ece33d61a8b6572a4b10d7b
Mireene-Unity <dhalsghkdxld@gmail.com>
2026-02-02 12:00:33 +0900

    [level 1] Title: 과일 장수, Time: 137.86 ms, Memory: 49.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Array.Sort(score);
+        Array.Reverse(score);
+        for(int i = m-1; i < score.Length; i += m)
+        {
+            answer += m * score[i];
+        }        
+        return answer;
+    }
+}
