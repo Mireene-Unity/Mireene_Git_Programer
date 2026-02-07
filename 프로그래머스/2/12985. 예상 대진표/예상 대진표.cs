105e32f37773dcf135d7e18105151b4d03c99728
Mireene-Unity <dhalsghkdxld@gmail.com>
2026-02-07 13:58:56 +0900

    [level 2] Title: 예상 대진표, Time: 0.18 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(a != b)
+        {
+            if(a % 2 ==1) a++;
+            if(b % 2 ==1) b++;
+            a /= 2;
+            b /= 2;
+            answer++;
+        }
+        return answer;
+    }
+}
