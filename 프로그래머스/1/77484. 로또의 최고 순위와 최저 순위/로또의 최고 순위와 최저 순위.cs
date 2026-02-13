9ddc3975aad503ff80fbfe909479353ca7c685fb
Mireene-Unity <dhalsghkdxld@gmail.com>
2026-02-13 13:10:47 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.28 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -5,10 +5,9 @@ public class Solution {
         int[] answer = new int[2]{7,7};
         foreach(var lotto in lottos)
         {
-            int zero = 0;
             if(lotto == 0)
             {
-               zero++;
+               answer[0]--;
             }
             foreach(var num in win_nums)
             {
@@ -18,7 +17,6 @@ public class Solution {
                     answer[1]--;
                 }
             }
-            answer[0] -= zero;
         }
         if(answer[1] > 6) answer[1] = 6;
         if(answer[0] > 6) answer[0] = 6;
